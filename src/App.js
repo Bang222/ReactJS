17587110ddaad341dfc3c2b0c9af996222c76d31
BangDanh <bangdanh105@gmail.com>
2022-11-24 21:40:24 +0700

    pull structure from main setup project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <ContainnerUser/>
             </header>
             <ToastContainer/>
         </div>
