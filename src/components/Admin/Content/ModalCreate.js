84f91fb5f1a8fd128b958c0e56dcd2399512e4e8
BangDanh <bangdanh105@gmail.com>
2022-11-25 10:53:57 +0700

    UpLoading Images done

--- src/components/Admin/Content/ModalCreate.js
+++ src/components/Admin/Content/ModalCreate.js
@@ -14,9 +14,17 @@ const ModalCreate= ()=> {
     const [Email,SetEmail] = useState('');
     const [Password,SetPassword] = useState('');
     const [UserName,SetUserName] = useState('');
+    const [Role,SetRole] = useState('User');
     const [Image,SetImage] = useState('');
+    const[PreviewImage,SetPreviewImage] = useState('');
 
-
+    const hanleUploadImage  =(event) =>{
+        if(event.target && event.target.files && event.target.files[0]) {
+            SetPreviewImage(URL.createObjectURL(event.target.files[0]))
+            SetImage(event.target.files[0])
+            return;
+        }
+    }
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
@@ -31,42 +39,54 @@ const ModalCreate= ()=> {
                 className='modal-create-user'
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>ADD NEW USER </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
                         <Row className="mb-3">
-                            <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Group as={Col} controlId="formGridCity">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control type="email" value={Email}
+                                       onChange={(event)=> SetEmail(event.target.value)}
+                                />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" value={Password}
+                                              onChange={(event)=> SetPassword(event.target.value)}
+                                />
                             </Form.Group>
                         </Row>
                         <Row className="mb-3">
                             <Form.Group as={Col} controlId="formGridCity">
                                 <Form.Label>User Name</Form.Label>
-                                <Form.Control />
+                                <Form.Control type="text" value={UserName}
+                                       onChange={(event)=> SetUserName(event.target.value)}
+                                />
                             </Form.Group>
 
                             <Form.Group as={Col} controlId="formGridState">
                                 <Form.Label>Role</Form.Label>
-                                <Form.Select defaultValue="Choose...">
-                                    <option>User</option>
-                                    <option>Admin</option>
+                                <Form.Select defaultValue="Role"
+                                             onChange={(event)=> SetRole(event.target.value)}>
+                                    <option value='User'>User</option>
+                                    <option value='Admin'>Admin</option>
                                 </Form.Select>
                             </Form.Group>
                             <div className="col-md-12 label-upload" >
                                 <label className="form-label content-label-upload" htmlFor='labelUpload'
                                 >  <FcPlus/> Upload Image</label>
-                                <input type='file' hidden id='labelUpload' />
+                                <input type='file' hidden id='labelUpload'
+                                onChange={(event) => hanleUploadImage(event)}
+                                />
                             </div>
                             <div className="col-md-12 preview-image ">
-                                <span className="content-preview-image"> Preview Image</span>
-                                <img  src={timer}/>
+                                {PreviewImage ?
+                                    <img  src={PreviewImage}/>
+                                    :
+                                    <span className="content-preview-image"> Preview Image</span>
+                                }
                             </div>
                         </Row>
                     </Form>
